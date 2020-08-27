848ccabbc052f131eeecbf56f3a9a065be874dca
kata-n <kata.kawa813@gmail.com>
2020-08-28 07:11:36 +0900

    ヘッダー作成

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="fixed top-0 bg-lightblue w-full max-w-sm h-16 py-4 z-10">
+    <div class="relative flex justify-center w-full">
+      <h2 class="text-2xl font-semibold text-white">Nuxt-chat-app</h2>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
 <template>
   <main class="container relative min-h-screen pt-16">
+    <Header />
     <Nuxt />
   </main>
 </template>
 
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 <style>
 html {
   font-family:
