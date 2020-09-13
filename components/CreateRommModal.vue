15dab52e9962cbed97c7bfc1c3e1c2547dcaed00
kata-n <kata.kawa813@gmail.com>
2020-09-13 19:15:36 +0900

    View作成

--- components/CreateRommModal.vue
+++ components/CreateRommModal.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="bg-white shadow rounded-lg m-4 w-11/12 p-4">
     <form @submit.prevnet="onSubmit">
-
+      <div class="flex justify-center items-center flex-col mt-3">
+        <div
+          :class="{ 'border border-red-500': form.imageUrl.errorMessage }"
+          class="rounded-lg w-32 h-32 bg-gray-200 flex items-center justify-center"
+        >
+        </div>
+      </div>
     </form>
   </div>
 </template>
