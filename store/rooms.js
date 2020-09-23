af6a2c4115dbf293721dad05d6ff1c73951a6ab0
kata-n <kata.kawa813@gmail.com>
2020-09-24 07:31:46 +0900

    room作成

--- store/rooms.js
+++ store/rooms.js
@@ -13,5 +13,22 @@ export const mutations = {
 
   if(isNotAdded) {
     state.rooms.push(romm)
+  },
+
+  update(state, { room }) {
+    state.rooms = state.rooms.map((r) => {
+      if (r.id === room.id) {
+        r = room
+      }
+      return r
+    })
+  },
+
+  remove(state, { room }) {
+    state.rooms = state.rooms.filter((r) => r.id !== room.id)
+  },
+
+  clear(state) {
+    state.rooms = []
   }
-},
+}
