30b9e311a8aa0ab128ec1a969fd7fae655c19c9a
kata-n <kata.kawa813@gmail.com>
2020-09-26 15:15:26 +0900

    ルーム作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <div>
+      <div
+        v-for="room in rooms"
+        :key="room.id"
+        class="bg-white max-w-sm rounded-lg overflow-hidden shadow m-4 mb-5 p-4 h-32"
+      >
+        <div>
+          <img
+            :src="room.topimageUrl"
+            class="float-left object-cover rounded-lg w-24 h-24 mr-4"
+          />
+          <p class="font-mono text-darkGray">{{ room.name }}</p>
+        </div>
+      </div>
+    </div>
     <div class="fixes flex justify-end bottom-0 w-full max-w-sm">
       <button @click="openModal">
         <i class="material-icons text-primary text-7xl create-btn gradation">
@@ -13,6 +28,7 @@
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import ModalBase from '~/components/ModalBase'
 import CreateRoomModal from '~/components/CreateRoomModal'
 
@@ -31,11 +47,15 @@ export default {
   methods: {
     openModal() {
       this.isCreateMode = true
-    }
-  },
+    }、
     closeModal() {
       this.isCreateMode = false
     }
+  },
+
+  computed: {
+    ...mapGetters('rooms',['rooms'])
+  }
 }
 </script>
 <style scoped>
