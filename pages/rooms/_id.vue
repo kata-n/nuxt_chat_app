73fcfb89000b1f595b1a01bf5d9c0f5ba2958d74
河村 泰佑 <kata.kawa813@gmail.com>
2020-10-09 08:23:12 +0900

    画面更新

--- pages/rooms/_id.vue
+++ pages/rooms/_id.vue
@@ -5,19 +5,19 @@
       @submit.prevent="onSubmit"
       class="fixed bottom-0 bg-white w-full max-w-sm flex py-4 border-t border-gray-300"
     >
-     <textarea
-       v-model="form.message.val"
-       placeholder="発言してみよう"
-       class="block appearance-none w-full ml-2 py-3 px-4 rounded-lg border border-gray-400 text-darkGray focus:outline-none focus:bg-white overflow-hidden bg-blue-100"
-       name="form.body"
-     />
-     <button
-      :disable="isValidateError"
-      :class="{ 'text-blue': !isValidateError }"
-      class="w-2/12 flex items-start justify-center text-gray font-semibold"
-     >
-     送信
-     </button>
+      <textarea
+        v-model="form.message.val"
+        placeholder="発言してみよう"
+        class="block appearance-none w-full ml-2 py-3 px-4 rounded-lg border border-gray-400 text-darkGray focus:outline-none focus:bg-white overflow-hidden bg-blue-100"
+        name="form.body"
+      />
+      <button
+        :disable="isValidateError"
+        :class="{ 'text-blue': !isValidateError }"
+        class="w-2/12 flex items-start justify-center text-gray font-semibold"
+      >
+        送信
+      </button>
     </form>
   </div>
 </template>
@@ -73,6 +73,12 @@ export default {
 
     scrollBottom(){
       const element = document.documentElement
+      const bottom = element.scrollHeight - elemant.clientHeight
+      window.scroll(0, bottom)
+    },
+
+    restForm() {
+      this.form.message.val = null
     }
   }
 }
