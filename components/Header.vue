9d892804db90d92e18414c0fdd37f61306e6263e
河村 泰佑 <kata.kawa813@gmail.com>
2020-10-11 23:46:43 +0900

    画面作成

--- components/Header.vue
+++ components/Header.vue
@@ -3,5 +3,13 @@
     <div class="relative flex justify-center w-full">
       <h2 class="text-2xl font-semibold text-white">Nuxt-chat-app</h2>
     </div>
+    <template v-if="isChatPage">
+      <nuxt-link
+        to="/"
+        class="absolute left-20 flex items-center justify-center no-underline"
+      >
+        <i class="material-icons text-4xl text-white">chevron_left</i>
+      </nuxt-link>
+    </template>
   </header>
 </template>
