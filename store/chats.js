37a111dd99f9c15d20ca747aba6a67503008c65f
河村 泰佑 <kata.kawa813@gmail.com>
2020-10-22 22:32:11 +0900

    チャット画面について

--- store/chats.js
+++ store/chats.js
@@ -1,3 +1,18 @@
 export const state = () => ({
   chats: []
 })
+
+export const getters = {
+  chats: (state) => state.chats
+}
+
+export const mutations = {
+  add(state, { chat }) {
+    const isEmpty = state.chats.length === 0
+    const isNotAdded = !state.chats.find((c) => c.id === chat.id)
+
+    if (isEmpty || isNotAdded) {
+      state.chats.push(chat)
+    }
+  }
+}
